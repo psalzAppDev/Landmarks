cd4423d1990ae86dec9cf6e36d577c9ce83fe1fe
Peter Salz <psalz.app.dev@web.de>
2020-07-07 21:24:41 +0200

    Added an EnvironmentObject user data to be shared across all views

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -10,17 +10,17 @@ import SwiftUI
 
 struct LandmarkList: View {
     
-    @State var showFavoritesOnly = true
+    @EnvironmentObject var userData: UserData
     
     var body: some View {
         
         NavigationView {
             List {
-                Toggle(isOn: $showFavoritesOnly) {
+                Toggle(isOn: $userData.showFavoritesOnly) {
                     Text("Favorites Only")
                 }
-                ForEach(landmarkData) { landmark in
-                    if !self.showFavoritesOnly || landmark.isFavorite {
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
                         NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                             LandmarkRow(landmark: landmark)
                         }
@@ -38,6 +38,7 @@ struct LandmarkList_Previews: PreviewProvider {
             LandmarkList()
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
+                .environmentObject(UserData())
         }
     }
 }
