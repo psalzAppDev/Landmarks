6e278f9f57a8f4267fd8f2e53ae3f2c62434d636
Peter Salz <psalz.app.dev@web.de>
2020-07-07 21:18:45 +0200

    Added UserData

---
+++ Landmarks/UserData.swift
@@ -0,0 +1,16 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Peter Salz on 07.07.20.
+//  Copyright © 2020 Peter Salz App Development. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
