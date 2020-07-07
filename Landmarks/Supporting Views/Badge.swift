3cd7d3815b9efa0785305a886cc0b43601146e59
Peter Salz <psalz.app.dev@web.de>
2020-07-07 21:42:43 +0200

    Added a Badge view

---
+++ Landmarks/Supporting Views/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Peter Salz on 07.07.20.
+//  Copyright © 2020 Peter Salz App Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
