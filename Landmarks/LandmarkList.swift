aa10c0878f06a2c43b19fd56055dbdb2571131b7
Peter Salz <psalz.app.dev@web.de>
2020-07-07 18:02:40 +0200

    Implemented a landmark list

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,25 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Peter Salz on 07.07.20.
+//  Copyright © 2020 Peter Salz App Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    
+    var body: some View {
+        
+        List(landmarkData) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
