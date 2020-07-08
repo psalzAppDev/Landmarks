ef80c1af603b34ab833fefcd9073b55a8021b0df
Peter Salz <psalz.app.dev@web.de>
2020-07-08 09:16:40 +0200

    Added a Hike View

---
+++ Landmarks/HikeView.swift
@@ -0,0 +1,57 @@
+//
+//  HikeView.swift
+//  Landmarks
+//
+//  Created by Peter Salz on 08.07.20.
+//  Copyright © 2020 Peter Salz App Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    
+    var hike: Hike
+    @State private var showDetail = false
+    
+    var body: some View {
+        
+        VStack {
+            HStack {
+                HikeGraph(hike: hike, path: \.elevation)
+                    .frame(width: 50, height: 30)
+                    .animation(nil)
+                
+                VStack(alignment: .leading) {
+                    Text(hike.name)
+                        .font(.headline)
+                    Text(hike.distanceText)
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    self.showDetail.toggle()
+                }) {
+                    Image(systemName: "chevron.right.circle")
+                        .imageScale(.large)
+                        .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        .padding()
+                }
+            }
+            
+            if showDetail {
+                HikeDetail(hike: hike)
+            }
+        }
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            HikeView(hike: hikeData[0])
+                .padding()
+            Spacer()
+        }
+    }
+}
